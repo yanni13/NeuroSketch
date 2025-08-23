730e27eab1c8adb47a4771685768402e4e61cec2
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-08-23 10:59:27 +0900

    backend project init

---
+++ Backend/src/main/kotlin/com/gtf/neuro_sketch/NeuroSketchApplication.kt
@@ -0,0 +1,11 @@
+package com.gtf.neuro_sketch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeuroSketchApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeuroSketchApplication>(*args)
+}
