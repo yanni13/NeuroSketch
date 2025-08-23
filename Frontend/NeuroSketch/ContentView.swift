6efe41c3b7b96d65db652fba7e02f848515e6366
heejinnn <gml0593@gmail.com>
2025-08-23 11:27:22 +0900

    chore: 프로젝트 초기 세팅

---
+++ Frontend/NeuroSketch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeuroSketch
+//
+//  Created by 최희진 on 8/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
